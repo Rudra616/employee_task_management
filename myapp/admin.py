cdbd724a5a59fcb27d5d2ed87edef71737cc285d
rudra <rudrampanchal@gmail.com>
2025-01-21 20:04:02 +0530

    commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import UserRegister
+from .models import *
 
 # Register your models here.
 
+
+
 class userreg_(admin.ModelAdmin):
-    list_display = ['id', 'name', 'email','password','mob', 'add']
-    # def Confirm_password(self, obj):
-    #     # Replace with actual logic to display password confirmation status
-    #     return 'Confirmed' if obj.password == obj.password_confirmation else 'Not Confirmed'
+    list_display = [ 'name', 'email','password','mob', 'add']
+    def Confirm_password(self, obj):
+        # Replace with actual logic to display password confirmation status
+        return 'Confirmed' if obj.password == obj.password_confirmation else 'Not Confirmed'
     
 admin.site.register(UserRegister, userreg_)
 
+admin.site.register(employeedetails)
--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,36 @@
 from django.db import models
-
 # Create your models here.
 
-class UserRegister(models.Model):
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.EmailField()
-    mob = models.TextField()
-    add = models.TextField()
+from django.core.validators import RegexValidator
 
-    
+class UserRegister(models.Model):
+    name = models.CharField(max_length=50, default="Default Name")  # Default added
+    password = models.CharField(max_length=50, default="default_password")  # Default added
+    email = models.EmailField(null=True, blank=True)  # Optional field
+    mob = models.CharField(
+        max_length=10,
+        default="1234567890",
+        validators=[
+            RegexValidator(
+                regex=r'^\d{10}$',
+                message="Mobile number must be 10 digits.",
+                code="invalid_mobile",
+            )
+        ],
+    )
+    add = models.TextField(default="Default Address")
+
     def __str__(self):
         return self.name
+class employeedetails(models.Model):
+
+    user = models.ForeignKey(UserRegister, on_delete=models.SET_NULL, null=True, blank=True)
+    empcode = models.CharField(max_length=50,null=True)
+    empdept = models.CharField(max_length=100,null=True)
+    destination = models.CharField(max_length=100,null=True)
+    contact = models.CharField(max_length=15,null=True)
+    gender = models.CharField(max_length=50,null=True)
+    joiningdate = models.DateField(null=True)
+
+    def __str__(self):
+        return self.user.name
